4036f188210a922eb600dd85aa33b4be4db71965
Hakurei <koray@atelieralice.net>
2025-08-06 11:50:47 +0300

    inital commint

---
+++ scripts/NewScript.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+public partial class NewScript : Node {
+    public override void _Ready() {
+        base._Ready();
+    }
+
+    public override void _Process(double delta) {
+        base._Process(delta);
+    }
+}
